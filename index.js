8a540f4602823cae08c00ce45e2f92536406a834
asdasd <149241521+ecrww@users.noreply.github.com>
2024-10-02 17:20:01 +0300

    mc
     So it's like the old one. but this is new

---
+++ index.js
@@ -0,0 +1,12 @@
+let ip = 3.39456 / 6;
+let eee;
+let asdasd;
+
+document.getElementById('asdasdp').onclick = function() {
+  eee = document.getElementById('asdasdq').value;
+  eee = Number(eee);
+
+  asdasd = 2 * ip * eee;
+
+  document.getElementById("aaaaaa").textContent = asdasd + " cm";
+};
